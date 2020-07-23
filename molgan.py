31cf63c2d1f5f59d51330e407e79c57ce6aae315
yanghongchao <873900820@qq.com>
2020-07-23 19:50:25 +0800

    update

--- molgan.py
+++ molgan.py
@@ -1,7 +1,10 @@
+import os
+import numpy as np
 from utils.sparse_molecular_dataset import SparseMolecularDataset
 from optimizers.gan import train_step
 from models.model import MolGan
 from config import args
+from utils.utils import mols2grid_image
 
 data = SparseMolecularDataset()
 data.load('data/gdb9_9nodes.sparsedataset')
@@ -37,10 +40,19 @@ def train_model(epochs):
                 train_g = False
             loss = train_step(data, model, args.la, train_g, feature_matching=True, batch_dim=args.batch_dim)
             print("step {step}, loss: {loss[0]}, {loss[1]}, {loss[2]} ".format(step=step, loss=loss))
+            if step%50 == 0:
+                predict("step-{}.bmp".format(step))
         model.save('data/model/molgan-{}.h5'.format(epoch))
 
-def predict():
-    pass
+
+def predict(figure_name):
+    nodes_gumbel_argmax, edges_gumbel_argmax = model(inputs=None, training=False)
+    n, e = nodes_gumbel_argmax, edges_gumbel_argmax
+    n, e = np.argmax(n, axis=-1), np.argmax(e, axis=-1)
+    gen_mols = [data.matrices2mol(n_, e_, strict=True) for n_, e_ in zip(n, e)]
+    smiles_figure = mols2grid_image(gen_mols, 3)
+    figure_path = os.path.join('data/figure/', figure_name)
+    smiles_figure.save(figure_path)
 
 
 def main():
